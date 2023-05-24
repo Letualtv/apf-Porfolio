b52fe9ca7a1eca58069efb96a5fce50d89478a14
Antonio P <106171750+Letualtv@users.noreply.github.com>
2023-05-24 21:44:30 +0200

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,55 @@
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Vuelve :(";
+})
+window.addEventListener("focus", () => {
+    document.title = "APDev";
+})
+
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
+
+  $(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+		
